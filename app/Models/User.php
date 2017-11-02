1aa6a3a31cdc9717aa2a9ca63131cc9e17b72d7c
unknown <will_sasaki@hotmail.com>
2017-11-02 18:57:21 -0200

    BodyPlate Laravel 5.4

---
+++ app/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+// class User extends Model implements Transformable
+class User extends Authenticatable implements Transformable
+{
+    use TransformableTrait;
+
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+}
